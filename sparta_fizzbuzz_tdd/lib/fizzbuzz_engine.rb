2cbc794d018b1ed99c61e7f22a39c36fce549f11
Tech-A38 <tech-a38@Tech-A38s-MacBook-Air.local>
2018-06-05 21:51:58 +0100

    fixed fizz buzz to return array now

--- sparta_fizzbuzz_tdd/lib/fizzbuzz_engine.rb
+++ sparta_fizzbuzz_tdd/lib/fizzbuzz_engine.rb
@@ -1,7 +1,26 @@
 class FizzbuzzEng
 
-  def method_name
+
+  def fizz_loop
+    hundred = []
+    for i in 1..100 do
+      if i % 15 == 0
+        hundred.push("FizzBuzz")
+      elsif i % 3 == 0
+        hundred.push("Fizz")
+      elsif i % 5 == 0
+        hundred.push("Buzz")
+      else
+        hundred.push(i)
+
+      end
+
+    end
+    return hundred
 
   end
+
+check = FizzbuzzEng.new
+check.fizz_loop
 
 end
--- sparta_fizzbuzz_tdd/spec/fizzbuzz_spec.rb
+++ sparta_fizzbuzz_tdd/spec/fizzbuzz_spec.rb
@@ -1,1 +1,17 @@
 require 'spec_helper'
+
+describe FizzbuzzEng do
+  before(:each) do
+    @fb = FizzbuzzEng.new
+  end
+
+  it 'should replace multiple of 3 and 5 with FizzBuzz' do
+    expect(@fb.fizz_loop[14]).to eq('FizzBuzz')
+  end
+
+
+
+
+
+
+end
